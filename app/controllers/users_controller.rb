d408582ceb388aadbe4226c6d763e65cbccc8b2f
Karen Olson <karmarie@gmail.com>
2021-11-23 20:54:26 -0600

    Completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def index 
+        render json: User.all
+    end
+
+    def show 
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
 end
